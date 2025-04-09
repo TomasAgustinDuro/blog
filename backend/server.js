f4bcb1ff17e5e749560df0cc21537a79e6483664
Tomas <durotomaas@gmail.com>
2025-04-08 22:28:01 -0300

    fix bug

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,7 @@ app.use(
 app.use("/login", loginRoute);
 
 app.get("/me", (req, res) => {
+  console.log(req.session);
   if (req.session.user) {
     console.log(req.session.user);
     res.json(req.session.user);
