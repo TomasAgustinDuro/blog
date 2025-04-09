a48b6c9f2bd7e52e0e7d84d8a19552b513ba3038
Tomas <durotomaas@gmail.com>
2025-04-09 14:24:26 -0300

    trying to fix a bug with cookies

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -25,6 +25,14 @@ export class Login {
       req.session.authenticated = true;
       req.session.user = { username: user };
 
+      req.session.save((err) => {
+        if (err) {
+          console.error("Error guardando sesión:", err);
+          return res.status(500).json({ error: "Error guardando sesión" });
+        }
+        return res.status(200).json("Sesión iniciada correctamente");
+      });
+
       // Verifica que la sesión se está configurando
       console.log("Sesión guardada:", req.session);
 
