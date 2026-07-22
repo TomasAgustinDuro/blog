efdd95d8d7d7fa374db1db227677317276539e4f
Tomas <durotomaas@gmail.com>
2026-07-22 20:19:27 -0300

    feat: restyling of frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import { AuthProvider } from "./auth/authContext";
-import AppRoutes from "../routes/routes";
+import AppRoutes from "./routes/routes";
 import Navbar from "./components/navbar";
 
 const queryClient = new QueryClient();
